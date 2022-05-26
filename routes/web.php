95aa1d810c747d77db3fbb8675cf7901530b2065
Tolga Bektaş <tolgabektas00@gmail.com>
2022-05-26 20:58:06 +0300

    route changed to index and verified middleware added

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('verified')->group(function () {
+    Route::get('/', function () {
+        return view('index');
+    });
 });
